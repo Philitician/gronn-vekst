b50687bb89ef223651bf8f0dcb32dcf685b04c53
Philip Rothschild <philiprene89@gmail.com>
2023-10-11 17:51:04 +0200

    🔧 chore(payload.config.ts): remove commented out graphQL schemaOutputFile configuration to clean up code 🐛 fix(page.tsx): add next.js tags to fetch request options to improve code readability and maintainability 🔧 chore(page.tsx): update className in main element to include gap-4 to add gap between flex items 🔧 chore(page.tsx): uncomment map function in products rendering to display products on the page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,23 +3,28 @@ import { env } from "@/env";
 const retrieveProducts = async () => {
   const productsUrl = `${env.PAYLOAD_API_BASE_URL}/products`;
   console.log(productsUrl);
-  const res = await fetch(productsUrl);
+  const res = await fetch(productsUrl, {
+    next: {
+      tags: ["products"],
+    },
+  });
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   return (await res.json()) as any[];
 };
 
 export default async function Home() {
   const products = await retrieveProducts();
+  console.log(products);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
       <h1 className="text-4xl font-bold">Products</h1>
       <ul className="flex flex-col space-y-4">
-        {/* {products?.map((product) => (
+        {products?.docs?.map((product) => (
           <li key={product.id}>
             <h2 className="text-2xl font-bold">{product.name}</h2>
             <p>{product.price}</p>
           </li>
-        ))} */}
+        ))}
       </ul>
     </main>
   );
